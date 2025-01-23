1cc6bcf8bc6494d7a54c9e0a3143c747aab334be
shutils <komorebi2614@gmail.com>
2025-01-23 22:32:40 +0900

    feat: create initial config file when first exec

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,22 +3,24 @@ package config
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"os"
 	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/BurntSushi/toml"
 	"github.com/adrg/xdg"
 )
 
-// AI model cost
-// $/M token
+// ModelCost represents the cost associated with AI model usage.
 type ModelCost struct {
 	Input, Output float64
 }
 
-// Configファイル情報
+const projectName = "lazyreview"
+
+// Config holds the configuration details for the application.
 type Config struct {
 	ConfigPath    string    `toml:"-"`
 	Key           string    `toml:"key"`
@@ -40,86 +42,131 @@ type Config struct {
 	Opener        string    `toml:"opener"`
 }
 
-var c Config
-
-func loadConfig(filePath string, config *Config) (Config, error) {
-	data, err := ioutil.ReadFile(filePath)
+// loadConfig reads the configuration from the specified file.
+func loadConfig(filePath string, config *Config) error {
+	data, err := os.ReadFile(filePath)
 	if err != nil {
-		return *config, err
+		return err
 	}
-	if _, err := toml.Decode(string(data), &config); err != nil {
-		return *config, err
-	}
-	return *config, nil
+	_, err = toml.Decode(string(data), config)
+	return err
 }
 
+// NewConfig initializes a new Config instance and populates it with values.
 func NewConfig() Config {
-	tFlagDefault := "."
-	oFlagDefault := "reviews.json"
-	mFlagDefault := "gpt-3.5-0125"
-	configPath := flag.String("config", "", "Path to the config file")
-	targetFlag := flag.String("target", tFlagDefault, "Target path")
-	outputFlag := flag.String("output", oFlagDefault, "Output path")
-	modelFlag := flag.String("model", mFlagDefault, "Model to use")
+	c := Config{}
+	defaults := map[string]string{
+		"target": ".",
+		"output": path.Join(xdg.DataHome, projectName, "reviews.json"),
+		"model":  "gpt-4o-mini",
+	}
 
+	configPath := flag.String("config", "", "Path to the config file")
 	flag.Parse()
 
-	// コンフィグファイルの読み込み
-	if *configPath != "" {
-		var err error
-		c, err = loadConfig(*configPath, &c)
-		if err != nil {
-			log.Fatalf("Failed to load config file: %v", err)
-		}
+	c.ConfigPath = determineConfigPath(*configPath)
+	setConfigFlags(&c, defaults)
+
+	if err := loadConfigFile(&c); err != nil {
+		saveConfig(c.ConfigPath, Config{})
 	}
 
-	// フラグ値でコンフィグ値を上書き（優先順位：フラグ > コンフィグファイル）
-	if *targetFlag != tFlagDefault || c.Target == "" {
-		c.Target = *targetFlag
-	}
-	if *outputFlag != oFlagDefault || c.Output == "" {
-		c.Output = *outputFlag
-	}
-	if *modelFlag != mFlagDefault || c.Model == "" {
-		c.Model = *modelFlag
-	}
+	validateConfig(&c)
 
-	// 必須フィールドの確認
-	if c.Target == "" || c.Output == "" || c.Model == "" {
-		log.Fatalf("Missing required configuration fields. Ensure `token`, `target`, `output`, and `model` are provided.")
-	}
-
-	if c.State == "" {
-		c.State = path.Join(xdg.StateHome, "lazyreview", "state.json")
-	}
-
-	c.ConfigPath = *configPath
-	c.TmpReviewPath = path.Join(xdg.CacheHome, "lazyreview", "tmp_review.md")
+	c.State = setDefaultState(c.State)
+	c.TmpReviewPath = path.Join(xdg.CacheHome, projectName, "tmp_review.md")
 
 	return c
 }
 
+func determineConfigPath(configPath string) string {
+	if configPath == "" {
+		return path.Join(xdg.ConfigHome, projectName, "config.toml")
+	}
+	return configPath
+}
+
+func setConfigFlags(c *Config, defaults map[string]string) {
+	for flagKey, defaultValue := range defaults {
+		switch flagKey {
+		case "target":
+			if val := flag.String("target", defaultValue, "Target path"); *val != defaultValue || c.Target == "" {
+				c.Target = *val
+			}
+		case "output":
+			if val := flag.String("output", defaultValue, "Output path"); *val != defaultValue || c.Output == "" {
+				c.Output = *val
+			}
+		case "model":
+			if val := flag.String("model", defaultValue, "Model to use"); *val != defaultValue || c.Model == "" {
+				c.Model = *val
+			}
+		}
+	}
+}
+
+func loadConfigFile(c *Config) error {
+	if c.ConfigPath == "" {
+		return nil
+	}
+	return loadConfig(c.ConfigPath, c)
+}
+
+func validateConfig(c *Config) {
+	if c.Target == "" || c.Output == "" || c.Model == "" {
+		log.Fatalf("Missing required configuration fields. Ensure `token`, `target`, `output`, and `model` are provided.")
+	}
+}
+
+func setDefaultState(state string) string {
+	if state == "" {
+		return path.Join(xdg.StateHome, projectName, "state.json")
+	}
+	return state
+}
+
+// ToStringArray converts the Config struct to a string array.
 func (c Config) ToStringArray() []string {
-	var result []string
-
-	// Replace Key value with asterisks
-	result = append(result, "key=******")
-
+	result := []string{"key=******"}
 	// Append other fields as key=value pairs
-	result = append(result, fmt.Sprintf("endpoint=%s", c.Endpoint))
-	result = append(result, fmt.Sprintf("version=%s", c.Version))
-	result = append(result, fmt.Sprintf("model=%s", c.Model))
-	result = append(result, fmt.Sprintf("target=%s", c.Target))
-	result = append(result, fmt.Sprintf("output=%s", c.Output))
-	result = append(result, fmt.Sprintf("state=%s", c.State))
-	result = append(result, fmt.Sprintf("ignores=%s", strings.Join(c.Ignores, ",")))
-	result = append(result, fmt.Sprintf("prompt=%s", c.Prompt))
-	result = append(result, fmt.Sprintf("type=%s", c.Type))
-	result = append(result, fmt.Sprintf("collector=%s", c.Collector))
-	result = append(result, fmt.Sprintf("glamour=%s", c.Glamour))
-	result = append(result, fmt.Sprintf("max_tokens=%d", c.MaxTokens))
-	result = append(result, fmt.Sprintf("tmp_review_path=%s", c.TmpReviewPath))
-	result = append(result, fmt.Sprintf("opener=%s", c.Opener))
-
+	result = append(result,
+		fmt.Sprintf("endpoint=%s", c.Endpoint),
+		fmt.Sprintf("version=%s", c.Version),
+		fmt.Sprintf("model=%s", c.Model),
+		fmt.Sprintf("target=%s", c.Target),
+		fmt.Sprintf("output=%s", c.Output),
+		fmt.Sprintf("state=%s", c.State),
+		fmt.Sprintf("ignores=%s", strings.Join(c.Ignores, ",")),
+		fmt.Sprintf("prompt=%s", c.Prompt),
+		fmt.Sprintf("type=%s", c.Type),
+		fmt.Sprintf("collector=%s", c.Collector),
+		fmt.Sprintf("glamour=%s", c.Glamour),
+		fmt.Sprintf("max_tokens=%d", c.MaxTokens),
+		fmt.Sprintf("tmp_review_path=%s", c.TmpReviewPath),
+		fmt.Sprintf("opener=%s", c.Opener),
+	)
 	return result
+}
+
+// saveConfig writes the Config data to a specified file.
+func saveConfig(filePath string, config Config) {
+	if filePath == "" {
+		log.Fatalf("File path is empty")
+	}
+
+	if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+		log.Fatalf("Failed to create directories: %v", err)
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Fatalf("Failed to create or open config file: %v", err)
+	}
+	defer file.Close()
+
+	if err := toml.NewEncoder(file).Encode(config); err != nil {
+		log.Fatalf("Failed to encode config to TOML: %v", err)
+	}
+
+	log.Printf("Config saved to %s", filePath)
 }
